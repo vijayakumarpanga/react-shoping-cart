1e152f2ce5592b71cc75cbe1ed45d997a7ab6b8b
VIJAYAKUMAR <pangavijaykumar9966@gmail.com>
2021-05-15 11:17:09 +0530

    Add Increase quantity and Decrease Quantity functions

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -10,6 +10,19 @@ class CartItem extends Component{
             qty    : 1
         }
     }
+    increaseQuantity=()=>{
+        this.setState((prevState)=>{
+           return  {qty : prevState.qty+1}
+        })
+    }
+    DecreaseQuantity=()=>{
+        this.setState((prevState)=>{
+            return  {qty : prevState.qty-1}
+         })
+    }
+    DeleteItem=()=>{
+        
+    }
     render(){
         const {title,price,qty}=this.state
         return(
@@ -29,9 +42,9 @@ class CartItem extends Component{
                      Qty : {qty}
                 </div>
                 <div className="actions">
-                <img className = "icons"  style={{height : 43,width:45}} alt="Increase" src="https://t3.ftcdn.net/jpg/02/51/03/82/240_F_251038282_CLb3d8tk99bGoU9ILEYS8vY215fgRmGT.jpg"></img>
-                    <img className = "" style={{height : 43,width:45}} alt="Decrease" src="https://t4.ftcdn.net/jpg/02/78/84/57/240_F_278845758_9xl3srVgd8p4jquxgxugGaHV1e5EOlLO.jpg"></img>
-                    <img className = "" style={{height : 38,width:35}} alt="Delete" src="https://t4.ftcdn.net/jpg/03/73/50/09/240_F_373500918_7vISJB85YXvvu7SgnpktP752LWRrLzyI.jpg"></img>
+                <img onClick="this.increaseQuantity" className = "icons"  style={{height : 43,width:45}} alt="Increase" src="https://t3.ftcdn.net/jpg/02/51/03/82/240_F_251038282_CLb3d8tk99bGoU9ILEYS8vY215fgRmGT.jpg"></img>
+                    <img  onClick="this.decreaseQuantity"className = "" style={{height : 43,width:45}} alt="Decrease" src="https://t4.ftcdn.net/jpg/02/78/84/57/240_F_278845758_9xl3srVgd8p4jquxgxugGaHV1e5EOlLO.jpg"></img>
+                    <img onClick="this.DeleteItem"className = "" style={{height : 38,width:35}} alt="Delete" src="https://t4.ftcdn.net/jpg/03/73/50/09/240_F_373500918_7vISJB85YXvvu7SgnpktP752LWRrLzyI.jpg"></img>
 
                   {//CartItem actions
                   }
